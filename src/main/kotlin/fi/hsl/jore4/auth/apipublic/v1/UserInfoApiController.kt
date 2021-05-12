7e25b6957f7e6382af099cbc3638c173c6cd6bf4
Jörg Schummer <jorg.schummer@vincit.fi>
2021-05-12 10:29:29 +0300

    Initial version of auth backend

---
+++ src/main/kotlin/fi/hsl/jore4/auth/apipublic/v1/UserInfoApiController.kt
@@ -0,0 +1,34 @@
+package fi.hsl.jore4.auth.apipublic.v1
+
+import fi.hsl.jore4.auth.userInfo.UserInfoService
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+/**
+ * Provides access to user info data.
+ */
+@RestController
+@RequestMapping("\${api.path.prefix}/public/v1")
+open class UserInfoApiController(
+    private val userInfoService: UserInfoService
+) : UserInfoApi {
+
+    companion object {
+        val LOGGER: Logger = LoggerFactory.getLogger(UserInfoApiController::class.java)
+    }
+
+    /**
+     * Retrieve the currently logged in user's user info.
+     */
+    override fun getUserInfo(): ResponseEntity<Map<String, Any>> {
+        LOGGER.info("Fetching user info of currently logged in user")
+
+        val userInfo = userInfoService.getUserInfo()
+        LOGGER.info("Returning user info: {}", userInfo)
+
+        return ResponseEntity.ok(userInfo)
+    }
+}
---
+++ src/main/kotlin/fi/hsl/jore4/auth/oidc/OIDCLoginService.kt
@@ -0,0 +1,79 @@
+package fi.hsl.jore4.auth.oidc
+
+import com.nimbusds.oauth2.sdk.ResponseType
+import com.nimbusds.oauth2.sdk.Scope
+import com.nimbusds.oauth2.sdk.id.ClientID
+import com.nimbusds.oauth2.sdk.id.State
+import com.nimbusds.openid.connect.sdk.AuthenticationRequest
+import fi.hsl.jore4.auth.apipublic.v1.OIDCCodeExchangeApiController
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Service
+import org.springframework.web.util.UriComponentsBuilder
+import java.net.URI
+import javax.servlet.http.HttpSession
+
+/**
+ * Login and logout functionality.
+ */
+@Service
+open class OIDCLoginService(
+    private val oidcProperties: OIDCProperties,
+    private val oidcProviderMetadataSupplier: OIDCProviderMetadataSupplier,
+    private val oidcCodeExchangeApiController: OIDCCodeExchangeApiController,
+    @Value("\${logoutpage.url}") private val logoutPageUrl: String
+) {
+    companion object {
+        private val LOGGER: Logger = LoggerFactory.getLogger(OIDCLoginService::class.java)
+
+        private const val OIDC_RESPONSE_TYPE = "code"
+        private val OIDC_SCOPES = arrayOf("openid", "profile", "externalpermissions.query")
+    }
+
+    /**
+     * Create the URI to which the user should be redirected in order to log in.
+     */
+    open fun createLoginUri(session: HttpSession): URI {
+        LOGGER.debug("Creating login URI...")
+
+        // generate a random state to verify the callback request
+        val state = State()
+
+        // generate the redirect URI at which the user will authenticate
+        val authRequestUri = AuthenticationRequest.Builder(
+            ResponseType(OIDC_RESPONSE_TYPE),
+            Scope(*OIDC_SCOPES),
+            ClientID(oidcProperties.clientId),
+            oidcCodeExchangeApiController.callbackUri
+        )
+            .endpointURI(oidcProviderMetadataSupplier.providerMetadata.authorizationEndpointURI)
+            .state(state)
+            .build()
+            .toURI()
+
+        session.setAttribute(SessionKeys.OIDC_STATE_KEY, state)
+
+        LOGGER.debug("Created login URI {}", authRequestUri)
+
+        return authRequestUri
+    }
+
+    /**
+     * Create the URI to which the user should be redirected in order to log out.
+     */
+    open fun createLogoutUri(): URI {
+        LOGGER.debug("Creating logout URI...")
+
+        val logoutUri = UriComponentsBuilder.fromUri(oidcProviderMetadataSupplier.providerMetadata.endSessionEndpointURI)
+            .queryParam("client_id", oidcProperties.clientId)
+            .queryParam("post_logout_redirect_uri", logoutPageUrl)
+            .build()
+            .encode()
+            .toUri()
+
+        LOGGER.debug("Created logout URI {}", logoutUri)
+
+        return logoutUri
+    }
+}
