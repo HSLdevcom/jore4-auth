5f49e2397d890b1796495d740806b9a1237b6c5f
Jörg Schummer <jorg.schummer@vincit.fi>
2021-07-28 19:50:43 +0300

    Use postgresl-persistent sessions
    Note that sessions are not persisted when running integrations tests.

--- src/integration-test/kotlin/fi/hsl/jore4/auth/IntegrationTestContext.kt
+++ src/integration-test/kotlin/fi/hsl/jore4/auth/IntegrationTestContext.kt
@@ -1,13 +1,27 @@
 package fi.hsl.jore4.auth
 
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Import
 import org.springframework.context.annotation.Primary
 import org.springframework.context.event.ContextRefreshedEvent
 import org.springframework.context.event.EventListener
 
+/**
+ * Integration context definitions.
+ *
+ * We don't use spring security, therefore exclude it from initialization.
+ *
+ * Neither do we use persistent sessions in the integration tests, therefore exclude
+ * the data source initialization.
+ */
 @Configuration
-@Import(Jore4AuthApplication::class)
+@SpringBootApplication(exclude = [
+    SecurityAutoConfiguration::class,
+    DataSourceAutoConfiguration::class, HibernateJpaAutoConfiguration::class
+])
 open class IntegrationTestContext {
 
     @EventListener(ContextRefreshedEvent::class)
--- src/main/kotlin/fi/hsl/jore4/auth/Jore4AuthApplication.kt
+++ src/main/kotlin/fi/hsl/jore4/auth/Jore4AuthApplication.kt
@@ -2,18 +2,14 @@ package fi.hsl.jore4.auth
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration
 
 /**
  * Spring boot application definition.
  *
- * Disable data source initialization, since we don't use a database at this stage. Similarly,
- * we don't use spring security, therefore exclude it from initialization.
+ * We don't use spring security, therefore exclude it from initialization.
  */
 @SpringBootApplication(exclude = [
-    DataSourceAutoConfiguration::class, HibernateJpaAutoConfiguration::class,
     SecurityAutoConfiguration::class
 ])
 open class Jore4AuthApplication
